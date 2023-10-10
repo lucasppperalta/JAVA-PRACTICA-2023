23d56925c1261f4f743a0f8ac6925a00e271abcd
Lucas Peralta <lp954982@gmail.com>
2023-10-10 17:26:15 -0300

    iniciando programacion estructurada

---
+++ JAVA_PROGRAMACION_ESTRUCTURADA/actividad1.java
@@ -0,0 +1,11 @@
+public class actividad1 {
+        /*1. Triangulo de asteriscos : Se le solicita que creen un programa que imprima un
+        triángulo de asteriscos en la consola. Usar bucles.*/
+    public static void main(String[] args) {
+        char asterisco = '*';
+        int contador = 0;
+        int i =;
+        for( i == contador ;); 
+
+}
+}
