47dc4b32e356b800dd54435e5a905545caf1ff11
lucasppperalta <105109674+lucasppperalta@users.noreply.github.com>
2023-10-11 22:05:40 -0300

    Delete Iniciado directory
    Iniciado delete

--- Iniciado/actividad_10.java
+++
@@ -1,5 +0,0 @@
-public class actividad_10 {
-    /*Acumulador: Se le solicita que guarde en un espacio de memoria la acumulación de quince multiplicaciones entre
-    dos números enteros divididos por el número de la multiplicación.
-      Por ejemplo: Numero1Numero2/1 + Numero1Numero2/2 + … + Numero1*Numero2/15 El resultado mostrarlo por pantalla. */
-}
